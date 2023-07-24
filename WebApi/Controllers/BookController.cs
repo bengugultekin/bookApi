d72c0ab19faeebaeaeecdca2440398ea297b783b
bengugultekin <bengunurgultekin54@gmail.com>
2023-07-24 22:39:57 +0300

    Added Entity Framework Core & DbContext

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -6,45 +6,55 @@ namespace WebApi;
 [Route("[controller]s")]
 public class BookController : ControllerBase
 {
-    private static List<Book> BookList = new List<Book>()
+
+    private readonly BookStoreDbContext dbContext;
+
+    public BookController (BookStoreDbContext dbContext)
     {
-        new Book
-        {
-            Id = 1,
-            Title = "Lean Startup",
-            GenreId = 1, // Personal Growth
-            PageCount = 200,
-            PublishDate = new DateTime(2001,06,12)
-        },
-        new Book
-        {
-            Id = 2,
-            Title = "Herland",
-            GenreId = 2, // Science Fiction
-            PageCount = 250,
-            PublishDate = new DateTime(2010,05,23)
-        },
-        new Book
-        {
-            Id = 3,
-            Title = "Dune",
-            GenreId = 2, // Science Fiction
-            PageCount = 540,
-            PublishDate = new DateTime(2001,12,21)
-        }
-    };
+        this.dbContext = dbContext;
+    }
+
+
+    //private static List<Book> BookList = new List<Book>()
+    //{
+    //    new Book
+    //    {
+    //        Id = 1,
+    //        Title = "Lean Startup",
+    //        GenreId = 1, // Personal Growth
+    //        PageCount = 200,
+    //        PublishDate = new DateTime(2001,06,12)
+    //    },
+    //    new Book
+    //    {
+    //        Id = 2,
+    //        Title = "Herland",
+    //        GenreId = 2, // Science Fiction
+    //        PageCount = 250,
+    //        PublishDate = new DateTime(2010,05,23)
+    //    },
+    //    new Book
+    //    {
+    //        Id = 3,
+    //        Title = "Dune",
+    //        GenreId = 2, // Science Fiction
+    //        PageCount = 540,
+    //        PublishDate = new DateTime(2001,12,21)
+    //    }
+    //};
+
 
     [HttpGet]
     public List<Book> GetBooks() 
     {
-        var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+        var bookList = dbContext.Books.OrderBy(x => x.Id).ToList<Book>();
         return bookList;
     }
 
     [HttpGet("{id}")]
     public Book GetById(int id)
     {
-        var book = BookList.Where(book => book.Id == id).SingleOrDefault();
+        var book = dbContext.Books.Where(book => book.Id == id).SingleOrDefault();
         return book;
     }
 
@@ -58,11 +68,12 @@ public class BookController : ControllerBase
     [HttpPost]
     public IActionResult AddBook([FromBody] Book newBook)
     {
-        var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+        var book = dbContext.Books.SingleOrDefault(x => x.Title == newBook.Title);
 
         if (book is not null)
             return BadRequest();
-        BookList.Add(newBook);
+        dbContext.Books.Add(newBook);
+        dbContext.SaveChanges();
         return Ok();
     }
 
@@ -70,7 +81,7 @@ public class BookController : ControllerBase
     [HttpPut("{id}")]
     public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
     {
-        var book = BookList.SingleOrDefault(x => x.Id == id);
+        var book = dbContext.Books.SingleOrDefault(x => x.Id == id);
 
         if(book is null)
             return BadRequest();
@@ -80,17 +91,19 @@ public class BookController : ControllerBase
         book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
         book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
 
+        dbContext.SaveChanges();
         return Ok();
     }
 
     [HttpDelete("{id}")]
     public IActionResult DeleteBook(int id) 
     {  
-        var book = BookList.SingleOrDefault(x => x.Id==id);
+        var book = dbContext.Books.SingleOrDefault(x => x.Id==id);
         if (book is null)
             return BadRequest();
 
-        BookList.Remove(book);
+        dbContext.Books.Remove(book);
+        dbContext.SaveChanges();
         return Ok(); 
     }
 
